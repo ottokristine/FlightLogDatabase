03581c641254787add8adf4787e6e132c3fcca90
Kristine Otto <ottokristine@gmail.com>
2020-03-28 20:11:02 -0400

    Added in file to drop all tables for easy recreation

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,28 +1,32 @@
---Create database CurrencyTracker;
+Create database CurrencyTracker
 
-CREATE TABLE Bulletin (
+CREATE TABLE CurrencyTracker.dbo.Bulletin (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(255)
 )
 
-CREATE TABLE Crew (
+CREATE TABLE CurrencyTracker.dbo.Crew (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
     Email Varchar(255),
     AdminFlag Int,
-    SiteLeadFlag INT,
     Constraint ckCrewAdminFlag check (AdminFlag in (0,1)),
-    Constraint ckSiteLeadFlag check (SiteLeadFlag in (0,1))
 )
 
-CREATE TABLE Acknowledges (
+CREATE TABLE CurrencyTracker.dbo.Site (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    CrewId INT FOREIGN KEY REFERENCES Crew(ID),
-    BulletinId INT FOREIGN KEY REFERENCES Bulletin(ID)
+    Name VARCHAR(255),
+    Prefix VARCHAR(255)
 )
 
-CREATE TABLE Requirement (
+CREATE TABLE CurrencyTracker.dbo.Acknowledges (
+    CrewId INT FOREIGN KEY REFERENCES Crew(ID),
+    BulletinId INT FOREIGN KEY REFERENCES Bulletin(ID),
+    PRIMARY KEY (CrewID,BulletinId)
+)
+
+CREATE TABLE CurrencyTracker.dbo.Requirement (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(255),
     DaysValid INT,
@@ -30,7 +34,7 @@ CREATE TABLE Requirement (
     Constraint requirementNeverExpires check (NeverExpiresFlag in (0,1))
 )
 
-CREATE TABLE FlightRequirement (
+CREATE TABLE CurrencyTracker.dbo.FlightRequirement (
     ID INT PRIMARY KEY FOREIGN KEY References Requirement(ID),
     RequiredLaunches INT,
     RequiredRecoveries INT,
@@ -38,44 +42,44 @@ CREATE TABLE FlightRequirement (
     RequiredMissionTime DECIMAL
 )
 
-CREATE TABLE Activity (
+CREATE TABLE CurrencyTracker.dbo.Activity (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(155)
 )
 
-CREATE TABLE RequirementActivities (
+CREATE TABLE CurrencyTracker.dbo.RequirementActivities (
     RequirementId INT FOREIGN KEY REFERENCES Requirement(ID),
     ActivityId Int FOREIGN KEY REFERENCES Activity(ID),
     PRIMARY KEY(RequirementId, ActivityId)
 )
 
-CREATE TABLE ROLE (
+CREATE TABLE CurrencyTracker.dbo.Role (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(255),
 )
 
-CREATE TABLE RoleRequirements (
+CREATE TABLE CurrencyTracker.dbo.RoleRequirements (
     RequirementID INT FOREIGN KEY REFERENCES Requirement(Id),
     RoleID INT FOREIGN KEY REFERENCES Role(Id),
     PRIMARY KEY(RequirementId, RoleID)
 )
 
-CREATE TABLE Mission (
+CREATE TABLE CurrencyTracker.dbo.Mission (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Site VARCHAR(255),
+    SiteId INT FOREIGN KEY REFERENCES Site(ID),
     Date DATETIME,
     LaunchNumber INT,
     AVTailNumber INT
 )
 
-CREATE TABLE LOG (
+CREATE TABLE CurrencyTracker.dbo.Log (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     Date DATETIME,
     ActivityId INT FOREIGN KEY REFERENCES Activity(ID),
     CrewID INT FOREIGN KEY REFERENCES Crew(ID)
 )
 
-CREATE TABLE FlightLog (
+CREATE TABLE CurrencyTracker.dbo.FlightLog (
     ID INT FOREIGN KEY REFERENCES Log(ID) PRIMARY KEY,
     MissionTime DECIMAL,
     Launch INT,
---
+++ DropAllTables.sql
@@ -0,0 +1,15 @@
+Use [CurrencyTracker]
+
+drop table flightlog
+drop table LOG
+drop table requirementActivities
+drop table activity
+drop table rolerequirements
+drop table role
+drop table requirement
+drop table acknowledges
+drop table bulletin
+drop table crew
+drop table Mission 
+drop table flightrequirement
+drop table requirement
